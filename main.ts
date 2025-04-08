c2632291493fc3d3829025761aa08bc330f5d117
Sam-MARTis <samanthmartis@gmail.com>
2025-04-08 10:45:54 +0530

    setup web structure

---
+++ main.ts
@@ -0,0 +1,8 @@
+
+const canvas = document.getElementById("projectCanvas") as HTMLCanvasElement;
+canvas.width = window.innerWidth*devicePixelRatio;
+canvas.height = window.innerHeight*devicePixelRatio;
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+if(!ctx){
+    throw Error("Context unable to be found");
+}
